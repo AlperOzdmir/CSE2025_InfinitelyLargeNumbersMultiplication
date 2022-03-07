39840da7c0f41bcf972547856e0229d64a2df0eb
Alper Özdemir <58814059+Tegralyn@users.noreply.github.com>
2022-03-08 01:06:32 +0300

    Add files via upload

---
+++ InfinitelyLargeNumber/cmake-build-debug/CMakeFiles/InfinitelyLargeNumber.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InfinitelyLargeNumber.dir/main.c.obj"
+  "InfinitelyLargeNumber.exe"
+  "InfinitelyLargeNumber.exe.manifest"
+  "InfinitelyLargeNumber.pdb"
+  "libInfinitelyLargeNumber.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/InfinitelyLargeNumber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
